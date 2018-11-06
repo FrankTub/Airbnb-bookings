3625319df6cca3d73d140275294144392e8f843a
Frank Tubbing <frank.tubbing@transfer-solutions.com>
2018-11-06 14:28:35 +0100

    Created a python file to store some functions

---
+++ utility.py
@@ -0,0 +1,15 @@
+# Create a scatter plot function
+def scatter_plot(df, kpi, column) :
+    '''
+    INPUT
+    df     - pandas dataframe 
+    kpi    - the variable you're interested in
+    column - interval variable
+    
+    OUTPUT
+    X - A scatter plot, with on the  x-axis the column and on the y-axis the kpi.
+    '''
+    plt.scatter(x=df[column], y=df[kpi])
+    plt.xlabel(column)
+    plt.ylabel(kpi)
+    return plt
